3ce007c3023bbe72b94b84e0b58f1fd1977fe935
RebeccaMillwood <bec-millwood@live.com.au>
2020-08-17 21:27:07 +1000

    re-wrote index, detail and results views with Django generic views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,56 +1,27 @@
-from django.http import HttpResponse, HttpResponseRedirect
-from django.template import loader
-
-from django.http import Http404
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
-
-from django.shortcuts import render
-
 from django.urls import reverse
+from django.views import generic
 
-from .models import Question
+from .models import Choice, Question
 
-# def index(request):
-#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-#     template = loader.get_template('polls/index.html')
-#     context = {
-#         'latest_question_list': latest_question_list,
-#     }
-#     return HttpResponse(template.render(context, request))
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-#re-written below with render() shortcut, once this is done
-#to all views, no longer need to import loader & HttpResponse
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
-# def detail(request, question_id):
-#     try:
-#         question = Question.objects.get(pk=question_id)
-#     except Question.DoesNotExist:
-#         raise Http404("Question does not exist")
-#     return render(request, 'polls/detail.html', {'question': question})
 
-#re-written with get_object_or_404() shortcut
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
-# def results(request, question_id):
-#     response = "You're looking at the results of question %s."
-#     return HttpResponse(response % question_id)
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
-# re-written to display results page after somebody votes in a question
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
-
-# def vote(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     return HttpResponse("You're voting on question %s." % question_id)
-
-# re-written - above was a 'dummy' function
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
@@ -67,4 +38,79 @@ def vote(request, question_id):
         # always return an HttpResponseRedicrect after successfully
         # dealing with POST data. This prevents data being posted twice
         # if a user hits the back button
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))    
+
+# 
+# updated views for generic view system. commented out original code below
+# 
+
+# from django.http import HttpResponse, HttpResponseRedirect
+# from django.template import loader
+
+# from django.http import Http404
+# from django.shortcuts import get_object_or_404, render
+
+# from django.shortcuts import render
+
+# from django.urls import reverse
+
+# from .models import Question
+
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     template = loader.get_template('polls/index.html')
+#     context = {
+#         'latest_question_list': latest_question_list,
+#     }
+#     return HttpResponse(template.render(context, request))
+
+#re-written below with render() shortcut, once this is done
+#to all views, no longer need to import loader & HttpResponse
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     context = {'latest_question_list': latest_question_list}
+#     return render(request, 'polls/index.html', context)
+
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     return render(request, 'polls/detail.html', {'question': question})
+
+#re-written with get_object_or_404() shortcut
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/detail.html', {'question': question})
+
+# def results(request, question_id):
+#     response = "You're looking at the results of question %s."
+#     return HttpResponse(response % question_id)
+
+# re-written to display results page after somebody votes in a question
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question': question})
+
+# def vote(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return HttpResponse("You're voting on question %s." % question_id)
+
+# re-written - above was a 'dummy' function
+# def vote(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     try:
+#         selected_choice = question.choice_set.get(pk=request.POST['choice'])
+#     except (KeyError, Choice.DoesNotExist):
+#         # re-display the question voting form
+#         return render(request, 'polls/detail.html',{
+#             'question': question,
+#             'error_message': "You didn't select a choice.",
+#         })
+#     else:
+#         selected_choice.votes += 1
+#         selected_choice.save()
+#         # always return an HttpResponseRedicrect after successfully
+#         # dealing with POST data. This prevents data being posted twice
+#         # if a user hits the back button
+#         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
