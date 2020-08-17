eafeb5156a2276fe914c94209afad07d17d40745
RebeccaMillwood <bec-millwood@live.com.au>
2020-08-17 20:30:29 +1000

    removed hardcoded URL

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -5,7 +5,9 @@ urlpatterns = [
     # ex: /polls/
     path('', views.index, name='index'),
     # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
+    # the 'name' value as called by the {% url %} template tag
+    # added the word 'specifics'
+    path('specifics/<int:question_id>/', views.detail, name='detail'),
     # ex: /polls/5/results/
     path('<int:question_id>/results/', views.results, name='results'),
     # ex: /polls/5/vote/
